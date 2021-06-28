35a695d4b19b91d854518fb9fdd7bd64f6163718
Maxim <skorokhodov.maxim.444@gmail.com>
2021-06-28 08:12:41 +0300

    realize classes for file saving

--- src/main/java/bsa/java/concurrency/fs/FileSystemRepository.java
+++ src/main/java/bsa/java/concurrency/fs/FileSystemRepository.java
@@ -1,19 +1,37 @@
 package bsa.java.concurrency.fs;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
 
 @Repository
 public class FileSystemRepository implements FileSystem {
 
-    @Value("${fs-root-directory}")
-    String rootDirectory;
+    Path rootDirectory;
+
+    @SneakyThrows
+    public FileSystemRepository(@Value("${fs-root-directory}") String rootDirectory) {
+        this.rootDirectory = Paths.get(rootDirectory);
+
+        if (!Files.exists(this.rootDirectory)) {
+            Files.createDirectories(this.rootDirectory);
+        }
+    }
 
     @Override
-    public CompletableFuture<String> saveFile(byte[] byteImage, String filename) {
-        return null;
+    public CompletableFuture<String> saveFile(byte[] byteImage, String name) {
+        return CompletableFuture.supplyAsync(() -> executeSaveToFile(byteImage, name));
+    }
+
+    @SneakyThrows
+    private String executeSaveToFile(byte[] byteImage, String name) {
+        var pathToFile = Path.of(rootDirectory.toString(), name + ".png");
+        return Files.write(pathToFile, byteImage).toString();
     }
 
 }
--- src/main/java/bsa/java/concurrency/image/ImageService.java
+++ src/main/java/bsa/java/concurrency/image/ImageService.java
@@ -2,6 +2,7 @@ package bsa.java.concurrency.image;
 
 import bsa.java.concurrency.fs.FileSystemService;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Arrays;
 import java.util.UUID;
 
+@Log4j2
 @Service
 public class ImageService {
 
@@ -24,7 +26,8 @@ public class ImageService {
         var byteImage = file.getBytes();
         var filename = UUID.randomUUID().toString() ;
 
-        fileSystemService.saveFile(byteImage, filename);
+        var pathToFile = fileSystemService.saveFile(byteImage, filename);
+        log.error(pathToFile);
     }
 
 }
